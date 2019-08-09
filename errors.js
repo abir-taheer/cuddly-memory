8a36a8d8256223c24edb70879262b87919aebcd5
Abir Taheer <abir488@gmail.com>
2019-08-09 16:50:37 +0600

    fixed error logging bug

--- errors.js
+++ errors.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
+const app_config = require("./config/app");
+const path = require("path");
 
 logError = (error, callback = () => {}) => {
-  fs.appendFile("./errors.txt", new Date().toUTCString() + " - " + error + "\n", callback);
+  fs.appendFile(app_config.root_path + "/errors.txt", new Date().toUTCString() + " - " + error + "\n", callback);
 };
 
 module.exports = logError;
