d9f7484ae54e0355bb9acf7fdfd121131a10314b
Abir Taheer <abir488@gmail.com>
2019-08-11 02:37:49 +0600

    displayed current games on my-games page

--- client/src/pages/MyGames.js
+++ client/src/pages/MyGames.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import socketIOClient from "socket.io-client";
+
+import {CircularProgress} from '@rmwc/circular-progress';
+import '@rmwc/circular-progress/circular-progress.css';
+
+import {Button} from "@rmwc/button";
+import '@material/button/dist/mdc.button.css';
+
+import {List, SimpleListItem} from "@rmwc/list";
+import '@material/list/dist/mdc.list.css';
+
+import {Card} from "@rmwc/card";
+import '@material/card/dist/mdc.card.css';
+
+
+// import socketIOClient from "socket.io-client";
+// const queryString = require('query-string');
 
 import {NavBar} from "../comp/NavBar";
+import {Spacer} from "../comp/Spacer";
 
-const queryString = require('query-string');
 
 export class MyGames extends React.Component {
   constructor(props) {
@@ -13,16 +28,26 @@ export class MyGames extends React.Component {
 
     this.state = {
       current_games: {
-
       },
-      fetched_latest: false
+      fetched_latest: false,
+      error: false
     };
 
-    fetch("/api/user/games")
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-        });
+    this.getGames = () => {
+      this.setState({error: false});
+      fetch("/api/user/games")
+          .then(res => res.json())
+          .then(data => {
+            this.setState({
+              current_games: data,
+              fetched_latest: true
+            });
+          })
+          .catch(() => {
+            this.setState({error: true});
+          });
+    };
+    this.getGames();
   }
 
   render() {
@@ -34,12 +59,58 @@ export class MyGames extends React.Component {
 
     // This page should only list the current games that the user is in based on their session data
     // And past games if the user is signed in
+    if(this.state.error){
+      return (
+          <div>
+            <NavBar/>
+            <h1 style={{color:"red"}}>Error Fetching Your Games</h1>
+            <div className={"flex-center"}>
+              <Button label="Try Again" danger outlined onClick={this.getGames}/>
+            </div>
+          </div>
+      )
+    }
+
+    if(!this.state.fetched_latest){
+      return (
+          <div>
+            <NavBar/>
+            <Spacer height={"5em"}/>
+            <CircularProgress size={"large"}/>
+          </div>
+      )
+    }
+
     return (
         <div>
           <NavBar/>
-          <h1>hi</h1>
+          <h1>My Current Games</h1>
+          <div className={"flex-center"}>
+            <Card className={"medium-width"}>
+              <div className={"sub-container"}>
+                <GamesList games={this.state.current_games}/>
+              </div>
+            </Card>
+          </div>
         </div>
     );
   }
 
+}
+
+class GamesList extends React.Component {
+  render() {
+    let items = this.props.games.map((data) =>
+        <SimpleListItem
+            graphic="touch_app"
+            text={data.game_name}
+            secondaryText={new Date(data.game_created_datetime).toDateString()}
+        />
+    );
+    return(
+        <List>
+          {items}
+        </List>
+    )
+  }
 }
